59b1fa5646306147668a42b20ee290beae3a0d64
Niraj Tiwari <tniraj7@gmail.com>
2024-12-18 00:28:44 +0900

    inject session to `ImageService`

--- test/ImageService.swift
+++ test/ImageService.swift
@@ -2,28 +2,31 @@ import Foundation
 import Combine
 
 class ImageService<S: Scheduler> {
+    typealias SchedulerType = S
+    
     private let scheduler: S
-    init(scheduler: S) {
+    private let session: URLSession
+    init(scheduler: S, session: URLSession = .shared) {
         self.scheduler = scheduler
+        self.session = session
     }
     
     func fetchImage(for url: String, completionHandler: @escaping (Data?) -> Void) {
-        let task = URLSession.shared
-            .dataTask(with: URLRequest(url: URL(string: url)!)) { data, res, error in
-                guard let safeData = data,
-                      let res = (res as? HTTPURLResponse),
-                      (200...299).contains(res.statusCode)
-                else {
-                    completionHandler(nil)
-                    return
-                }
-                completionHandler(safeData)
+        let task = session.dataTask(with: URLRequest(url: URL(string: url)!)) { data, res, error in
+            guard let safeData = data,
+                  let res = (res as? HTTPURLResponse),
+                  (200...299).contains(res.statusCode)
+            else {
+                completionHandler(nil)
+                return
             }
+            completionHandler(safeData)
+        }
         task.resume()
     }
     
     func fetchImage(for url: String) -> AnyPublisher<Data, CustomError> {
-        return URLSession.shared.dataTaskPublisher(for: URL(string: url)!)
+        return session.dataTaskPublisher(for: URL(string: url)!)
             .tryMap { (data: Data?, response: URLResponse) in
                 guard let safeData = data,
                       let res = (response as? HTTPURLResponse),
@@ -39,7 +42,7 @@ class ImageService<S: Scheduler> {
     }
     
     func fetchImage(for url: String) async throws -> Data {
-        let (data, res) = try await URLSession.shared.data(from: URL(string: url)!)
+        let (data, res) = try await session.data(from: URL(string: url)!)
         guard 
             let res = (res as? HTTPURLResponse),
             (200...299).contains(res.statusCode)
